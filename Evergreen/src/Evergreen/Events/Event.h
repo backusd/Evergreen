8e1b208f21de44bd53c94e3f5e27bec4407e5563
Dakota Backus <backusdakota@gmail.com>
2022-11-08 11:22:29 -0800

    Cleanup

--- Evergreen/src/Evergreen/Events/Event.h
+++ Evergreen/src/Evergreen/Events/Event.h
@@ -43,34 +43,6 @@ protected:
 	bool m_handled = false;
 };
 
-/*
-class EVERGREEN_API EventDispatcher
-{
-	template<typename T>
-	using EventFn = std::function<bool(T&)>;
-
-public:
-	EventDispatcher(Event& event) noexcept :
-		m_event(event) {}
-	EventDispatcher(const EventDispatcher&) = delete;
-	void operator=(const EventDispatcher&) = delete;
-
-	template<typename T>
-	bool Dispatch(EventFn<T> func) noexcept
-	{
-		if (m_event.GetEventType() == T::GetStaticType())
-		{
-			m_event.m_handled = func(*(T*)&m_event);
-			return true;
-		}
-		return false;
-	}
-
-private:
-	Event& m_event;
-};
-*/
-
 inline std::ostream& operator<<(std::ostream& os, const Event& e)
 {
 	return os << e.ToString();
