5707e637d9324d1dd9898049161084a1d7cddeae
Dakota Backus <backusdakota@gmail.com>
2022-11-08 11:19:43 -0800

    Removed use of std::bind

--- Evergreen/src/Evergreen/Application.cpp
+++ Evergreen/src/Evergreen/Application.cpp
@@ -1,33 +1,30 @@
 #include "pch.h"
 #include "Application.h"
-
-#include "Evergreen/Events/ApplicationEvent.h"
-#include "Evergreen/Events/KeyEvent.h"
 #include "Evergreen/Log.h"
 
 namespace Evergreen
 {
-#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+#define BIND_EVENT_FN(fn, type) [this](type& e) { this->fn(e); }
 
 Application::Application() noexcept
 {
 	m_window = std::unique_ptr<Window>(Window::Create());
-	m_window->SetOnWindowResize(BIND_EVENT_FN(OnWindowResize));
-	m_window->SetOnWindowCreate(BIND_EVENT_FN(OnWindowCreate));
-	m_window->SetOnWindowClose(BIND_EVENT_FN(OnWindowClose));
-	m_window->SetOnAppTick(BIND_EVENT_FN(OnAppTick));
-	m_window->SetOnAppUpdate(BIND_EVENT_FN(OnAppUpdate));
-	m_window->SetOnAppRender(BIND_EVENT_FN(OnAppRender));
-	m_window->SetOnChar(BIND_EVENT_FN(OnChar));
-	m_window->SetOnKeyPressed(BIND_EVENT_FN(OnKeyPressed));
-	m_window->SetOnKeyReleased(BIND_EVENT_FN(OnKeyReleased));
-	m_window->SetOnMouseMove(BIND_EVENT_FN(OnMouseMove));
-	m_window->SetOnMouseEnter(BIND_EVENT_FN(OnMouseEnter));
-	m_window->SetOnMouseLeave(BIND_EVENT_FN(OnMouseLeave));
-	m_window->SetOnMouseScrolled(BIND_EVENT_FN(OnMouseScrolled));
-	m_window->SetOnMouseButtonPressed(BIND_EVENT_FN(OnMouseButtonPressed));
-	m_window->SetOnMouseButtonReleased(BIND_EVENT_FN(OnMouseButtonReleased));
-	m_window->SetOnMouseButtonDoubleClick(BIND_EVENT_FN(OnMouseButtonDoubleClick));
+	m_window->SetOnWindowResize(BIND_EVENT_FN(OnWindowResize, WindowResizeEvent));
+	m_window->SetOnWindowCreate(BIND_EVENT_FN(OnWindowCreate, WindowCreateEvent));
+	m_window->SetOnWindowClose(BIND_EVENT_FN(OnWindowClose, WindowCloseEvent));
+	m_window->SetOnAppTick(BIND_EVENT_FN(OnAppTick, AppTickEvent));
+	m_window->SetOnAppUpdate(BIND_EVENT_FN(OnAppUpdate, AppUpdateEvent));
+	m_window->SetOnAppRender(BIND_EVENT_FN(OnAppRender, AppRenderEvent));
+	m_window->SetOnChar(BIND_EVENT_FN(OnChar, CharEvent));
+	m_window->SetOnKeyPressed(BIND_EVENT_FN(OnKeyPressed, KeyPressedEvent));
+	m_window->SetOnKeyReleased(BIND_EVENT_FN(OnKeyReleased, KeyReleasedEvent));
+	m_window->SetOnMouseMove(BIND_EVENT_FN(OnMouseMove, MouseMoveEvent));
+	m_window->SetOnMouseEnter(BIND_EVENT_FN(OnMouseEnter, MouseEnterEvent));
+	m_window->SetOnMouseLeave(BIND_EVENT_FN(OnMouseLeave, MouseLeaveEvent));
+	m_window->SetOnMouseScrolled(BIND_EVENT_FN(OnMouseScrolled, MouseScrolledEvent));
+	m_window->SetOnMouseButtonPressed(BIND_EVENT_FN(OnMouseButtonPressed, MouseButtonPressedEvent));
+	m_window->SetOnMouseButtonReleased(BIND_EVENT_FN(OnMouseButtonReleased, MouseButtonReleasedEvent));
+	m_window->SetOnMouseButtonDoubleClick(BIND_EVENT_FN(OnMouseButtonDoubleClick, MouseButtonDoubleClickEvent));
 }
 
 Application::~Application() noexcept
